174ac4601324b6c8235ef249150fe99f96339fad
Fyordo <lagutinfedya@gmail.com>
2022-10-13 09:16:49 +0300

    Merge master to main

---
+++ lib/jsdg.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "jsdg/version"
+
+module Jsdg
+  class Error < StandardError; end
+  # Your code goes here...
+  extend self
+
+  def hello()
+    'hello'
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "jsdg"
+
+require "minitest/autorun"
