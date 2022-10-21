9940d99116444e74cac88fe4b2aebf838002b367
IDante <maxim.polyanskiy@mail.ru>
2022-10-21 22:48:22 +0300

    Add dependences

--- lib/jsdg.rb
+++ lib/jsdg.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 
 require_relative "jsdg/version"
+require 'prawn'
 
 module Jsdg
-  class Error < StandardError; end
-  # Your code goes here...
-  extend self
 
-  def hello()
-    'hello'
-  end
+	class Error < StandardError; end
+	# Your code goes here...
+	extend self
+
+	def hello()
+# Using explicit block form and rendering to a file
+
+	content = "Hello World"
+	Prawn::Document.generate "example.pdf" do |pdf|
+
+# self here is left alone
+	pdf.font "Times-Roman"
+	pdf.draw_text content, :at => [200,720], :size => 32
+
+        end
+    end
 end
